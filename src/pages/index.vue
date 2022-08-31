82f974dce0f97c68f3ac4784f0aa48f81e65adbe
brian4159 <88099867+brian4159@users.noreply.github.com>
2022-08-31 23:08:46 +0800

    login

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
         后台首页
+        <el-button @click="addCount">{{count }}</el-button>
+        <el-button type="primary" @click="addFormCount">{{form.count }}</el-button>
     </div>
 </template>
+<script setup>
+    import { ref ,reactive}  from 'vue'
+     let count = ref(1);
+    let form = reactive({
+        count:2
+    })
+
+     function addCount(){
+        console.log(count.value ++)
+     }
+     function addFormCount(){
+        console.log(form.count ++)
+     }
+</script>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,58 +1,128 @@
 <template>
-  <div>
-    <el-row class="bg-indigo-500 min-h-screen">
-      <el-col :lg="16" :md="12" class="flex items-center justify-center">
-        <div>
-          <div class="font-bold text-5xl text-light-50 mb-4">欢迎光临</div>
-          <div class="text-gray-200 text-sm">小王同学,秋招必胜^_^<span></span></div>
-        </div>
+  <el-row class="login-container">
+      <el-col :lg="16" :md="12" class="left">
+          <div>
+              <div>欢迎光临</div>
+              <div>小王同学，秋招必胜^_^</div>
+          </div>
       </el-col>
-      <el-col
-        :lg="8" :md="12"
-        class="bg-light-50 flex-col flex items-center justify-center"
-      >
-        <h2 class="font-bold text-3xl  text-gray-800 ">欢迎回来</h2>
-        <div class="flex items-center justify-center my-5 text-gray-300 space-x-2">
-            <span class="h-[1px] w-16 bg-gray-200"></span>
-            <span class="mb-3">账号密码登录</span>
-            <span  class="h-[1px] w-16 bg-gray-200"></span>
-        </div>
-        <el-form :model="form" class="w-[250px]">
-          <el-form-item >
-            <el-input v-model="form.username" placeholder="请输入用户名">
-              <template #prefix>
-                <el-icon><user /></el-icon>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item >
-            <el-input v-model="form.password" placeholder="请输入密码">
-              <template #prefix>
-                <el-icon><lock/></el-icon>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button  round color="#626aef" type="primary" @click="onSubmit" class="w-[250px]">登录</el-button>
-            
-          </el-form-item>
-        </el-form>
+      <el-col :lg="8" :md="12" class="right">
+          <h2 class="title">欢迎回来</h2>
+          <div>
+              <span class="line"></span>
+              <span>账号密码登录</span>
+              <span class="line"></span>
+          </div>
+          <el-form ref="formRef" :rules="rules" :model="form" class="w-[250px]">
+              <el-form-item prop="username">
+                  <el-input v-model="form.username" placeholder="请输入用户名">
+                      <template #prefix>
+                          <el-icon><user /></el-icon>
+                      </template>
+                  </el-input>
+              </el-form-item>
+              <el-form-item prop="password">
+                  <el-input type="password" v-model="form.password" placeholder="请输入密码" show-password>
+                      <template #prefix>
+                          <el-icon><lock /></el-icon>
+                      </template>
+                  </el-input>
+              </el-form-item>
+              <el-form-item>
+                  <el-button round color="#626aef" class="w-[250px]" type="primary" @click="onSubmit">登 录</el-button>
+              </el-form-item>
+          </el-form>
       </el-col>
-    </el-row>
-  </div>
+  </el-row>
 </template>
 
-<script  setup>
-import { reactive } from "vue";
+<script setup>
+import { ref,reactive } from 'vue'
+import { ElNotification } from 'element-plus'
+import { useRouter } from 'vue-router'
+import { login } from '~/api/manager'
+
+const router = useRouter()
 
 // do not use same name with ref
 const form = reactive({
-  username: "",
-  password: "",
- 
-});
+username:"",
+password:""
+})
+
+const rules = {
+  username:[
+      { 
+          required: true, 
+          message: '用户名不能为空', 
+          trigger: 'blur' 
+      },
+  ],
+  password:[
+      { 
+          required: true, 
+          message: '用户名不能为空', 
+          trigger: 'blur' 
+      },
+  ]
+}
+
+const formRef = ref(null)
 
 const onSubmit = () => {
-  console.log("submit!");
-};
+  formRef.value.validate((valid)=>{
+      if(!valid){
+          return false
+      }
+      login(form.username,form.password)
+      .then(res=>{
+
+          // 提示成功
+          ElNotification({
+              message: "登录成功",
+              type: 'success',
+              duration:3000
+          })
+
+          // 存储token和用户相关信息，下节课讲
+
+          // 跳转到后台首页
+          router.push("/")
+      })
+      .catch(err=>{
+          ElNotification({
+              message: err.response.data.msg || "请求失败",
+              type: 'error',
+              duration:3000
+          })
+      })
+  })
+}
 </script>
+
+<style scoped>
+.login-container{
+  @apply min-h-screen bg-indigo-500;
+}
+.login-container .left,.login-container .right{
+  @apply flex items-center justify-center;
+}
+.login-container .right{
+  @apply bg-light-50 flex-col;
+}
+.left>div>div:first-child{
+  @apply font-bold text-5xl text-light-50 mb-4;
+}
+.left>div>div:last-child{
+  @apply text-gray-200 text-sm;
+}
+.right .title{
+  @apply font-bold text-3xl text-gray-800;
+}
+.right>div{
+  @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
+}
+.right .line{
+  @apply h-[1px] w-16 bg-gray-200;
+}
+</style>
